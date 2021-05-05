bb1b9cfdd67c79a88de14d1939b11fb199fe8f76
Rekha-1 <maneeta808@gmail.com>
2021-05-05 09:05:55 +0530

    style changes

--- script.js
+++ script.js
@@ -12,10 +12,7 @@ function btnclick(id) {
     if (count % 2 != 0) {
       if (currentTurn == X_Turn && filled !== "true") {
         button.innerText = "X";
-<<<<<<< HEAD
         button.style.fontFamily="Patrick Hand, cursive";
-=======
->>>>>>> ca6112a96b93d6aa9b4ef3fb92ae48724fa4c882
         button.style.color = "white";
         currentTurn = O_Turn;
         button.dataset.clicked = "true";
@@ -29,10 +26,7 @@ function btnclick(id) {
     else {
       if (currentTurn == O_Turn && filled !== "true") {
         button.innerText = "O";
-<<<<<<< HEAD
         button.style.fontFamily="Patrick Hand, cursive";
-=======
->>>>>>> ca6112a96b93d6aa9b4ef3fb92ae48724fa4c882
         button.style.color = "rgb(167, 85, 52)";
         currentTurn = X_Turn;
         button.dataset.clicked = "true";
@@ -89,11 +83,9 @@ function checkCondition(id1, id2, id3) {
 function getData(id) {
   return document.getElementById(id).innerText;
 }
-<<<<<<< HEAD
 // gameState.some(e=> {
 //   if(e===""){
 //     console.log('draw')
 //   }
 // })
-=======
->>>>>>> ca6112a96b93d6aa9b4ef3fb92ae48724fa4c882
+
